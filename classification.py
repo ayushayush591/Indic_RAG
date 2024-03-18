d5691f9acb818c2810670e1ee6d7812607f6f826
Abhishek kumar singh <54285340+ayushayush591@users.noreply.github.com>
2024-03-18 15:40:22 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -25,7 +25,7 @@ prompt="""<s>[INST] <<SYS>>
 You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
 
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-<</SYS>>""" + "Please Answer Just Yes or No based on whether the question can Exactly be answered by Given Paragraph:\n"
+<</SYS>>""" + "Please Answer Just Yes or No based on whether the question can Exactly be answered by Given Context:\n"
 
 for i in range(len(context)):
     prompts.append(f"{prompt} context:\n{context[i]}\n question:\n {question[i]}\nAnswer:[/INST]")
